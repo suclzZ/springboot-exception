dc856cae0d52d6c68fcc2f81b8b533c7da17f423
suspring <sky_tk@126.com>
2019-08-10 12:49:25 +0800

    'add'

--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
-1、404错误如果不处理，会调用springboot默认机制
-2、500错误，通过自定义HandlerExeceptionResolver
-3
-
  [参考](https://www.cnblogs.com/FlyAway2013/p/7944568.html)
+ 
+ 1、默认情况下springboot异常处理在ErrorMvcAutoConfiguration配置，主要是通过其内置的tomcat完成，针对内部异常，将错误指向/error->BasicErrorController进行处理，
+ 但是要注意，同时通过BeanNameViewer这个视图解析器来完成error最终的显示。要注意的是，这个处理内部异常；基于内置tomcat异常。
+ 2、通过继承AbstractErrorController可以覆盖BasicErrorController，改写其默认逻辑
+ 3、通过重写ErrorAttributes可以在BasicErrorController的处理逻辑上修改返回值
+ 4、通过实现HandlerExceptionResolver可以覆盖默认的异常处理器
+ 5、通过@ControllerAdvice可以实现Controller的异常统一处理
+ 6、通过注册EmbeddedServletContainerCustomizer可以根据不同响应错误定位不同处理页面比如404、500...
+ 
--- src/main/java/com/sucl/exception/SpringbootExceptionApplication.java
+++ src/main/java/com/sucl/exception/SpringbootExceptionApplication.java
@@ -1,13 +1,27 @@
 package com.sucl.exception;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.AbstractErrorController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Map;
 
 @SpringBootApplication
-public class SpringbootExceptionApplication {
+public class SpringbootExceptionApplication implements CommandLineRunner {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootExceptionApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, AbstractErrorController> c = applicationContext.getBeansOfType(AbstractErrorController.class);
+        System.out.println(c);
+    }
 }
